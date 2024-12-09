d53cddd34f7782d020594ee3a3403584dec30229
njtech <0syddyinl@mozmail.com>
2024-12-09 09:09:36 +0530

    add install and uninstall script

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+mkdir -p ~/.local/bin/
+cp ./xfce-super-key-fix.sh ~/.local/bin/
+cp ./xfce-super-key-fix.desktop ~/.config/autostart/
+sed -i -e 's/<EXECUTABLE>/~\/.local\/bin\/xfce-super-key-fix.sh/g' ~/.config/autostart/xfce-super-key-fix.desktop
---
+++ uninstall.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+rm ~/.local/bin/xfce-super-key-fix.sh
+rm ~/.config/autostart/xfce-super-key-fix.desktop
